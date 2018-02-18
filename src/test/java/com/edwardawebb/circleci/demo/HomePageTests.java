c80ea173c1f1bf321a0dc88c38810c98daa618fa
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2018-02-18 14:30:16 -0500

    Adding circleci config and phantomJS testsnot quite sure this phantomjs install will work, will use this build to live debug circle CI nodes and update manifest as needed

--- src/test/java/com/edwardawebb/circleci/demo/HomePageTests.java
+++ src/test/java/com/edwardawebb/circleci/demo/HomePageTests.java
@@ -4,13 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,35 +20,42 @@ import static org.hamcrest.core.Is.is;
 @RunWith(SpringRunner.class)
 public class HomePageTests {
 
-
-
-    @org.springframework.boot.test.context.TestConfiguration
-    static class TestConfiguration {
-
-        @Bean
-        public WebDriver webDriver(){
-            return new PhantomJSDriver();
-        }
-    }
-
-
     @LocalServerPort
     private int serverPort;
-
     @Autowired
     WebDriver webDriver;
 
 
     @Test
-    public void testButtonShowsInfoDialog() throws Exception {
-            webDriver.get(getUrl());
-            assertThat("Did not find welcome text",webDriver.findElement(By.id("welcome")).getText(),is("Hello Circle CI!"));
+    public void testHomePageLoads() throws Exception {
+        webDriver.get(getUrl());
+        assertThat("Did not find welcome text",webDriver.findElement(By.id("welcome")).getText(),is("Hello Circle CI!"));
+    }
+
+    @Test
+    public void testButtonDisplaysInfoDialog() throws Exception {
+        webDriver.get(getUrl());
+        WebElement infoDetails = webDriver.findElement(By.id("msgBox"));
+        WebElement infoButton = webDriver.findElement(By.id("msgButton"));
+        assertThat("Hidden element visible by default.", infoDetails.isDisplayed(),is(false));
+        infoButton.click();
+        assertThat("Additional info not displayed on button click.",infoDetails.isDisplayed(),is(true));
     }
 
     private String getUrl() {
         return "http://localhost:" + serverPort;
     }
 
+
+    @org.springframework.boot.test.context.TestConfiguration
+    static class TestConfiguration {
+        @Bean
+        public WebDriver webDriver(){
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setJavascriptEnabled(true);
+            return new PhantomJSDriver(capabilities);
+        }
+    }
 
 
 }
